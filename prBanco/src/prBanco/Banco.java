4de5c4e00d573a9f5b2d89059dc5b06d1ee57603
unknown <adryzg00@gmail.com>
2023-04-24 11:07:41 +0200

    Implementacion

--- prBanco/src/prBanco/Banco.java
+++ prBanco/src/prBanco/Banco.java
@@ -1,5 +1,17 @@
 package prBanco;
 
+/**
+ * @author Adri
+ *
+ */
 public class Banco {
-
+	
+	private int iban;
+	private double saldo;
+	
+	public Banco(int iban, double saldo) {
+		this.iban = iban;
+		this.saldo = saldo;
+	}
+	
 }
