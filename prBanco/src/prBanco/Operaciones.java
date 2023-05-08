a71e3d4c34490bcecd2d5d46c3b366f8c4494b36
OscarNunez19 <oscarcolina6@gmail.com>
2023-05-08 10:37:52 +0200

    metodo transferencia

--- prBanco/src/prBanco/Operaciones.java
+++ prBanco/src/prBanco/Operaciones.java
@@ -4,4 +4,9 @@ public abstract class Operaciones {
 	public void Prestamo(Cuenta c, double cantidad) {
 		c.setSaldo(c.getSaldo()+cantidad);
 		}
+	
+	public void Transferencia(Cuenta c1, Cuenta c2, double cantidad) {
+		c2.setSaldo(cantidad+c2.getSaldo());
+		c1.setSaldo(c1.getSaldo()-cantidad);
+	}
 }
