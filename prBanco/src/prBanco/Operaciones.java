9171444e46bfba9cd4526d377a45df58adb8518c
unknown <migue.rueda.90@gmail.com>
2023-05-08 10:43:07 +0200

    Metodos activar y desactivar cuenta corriente

--- prBanco/src/prBanco/Operaciones.java
+++ prBanco/src/prBanco/Operaciones.java
@@ -9,4 +9,21 @@ public abstract class Operaciones {
 		c2.setSaldo(cantidad+c2.getSaldo());
 		c1.setSaldo(c1.getSaldo()-cantidad);
 	}
+	/**
+	 * Metodo para cambiar el estado de una cuenta a bloqueado
+	 *Recibe por parametro un objeto de clase Cuenta
+	 *No devuelve nada. Cambia el estado del objeto Cuenta a "Bloqueado"
+	 */
+	public void bloquearCuenta(Cuenta cuenta) {
+		cuenta.setEstado("Bloqueada");
+	}
+	/**
+	 * Metodo para cambiar el estado de una cuenta a activada
+	 *Recibe por parametro un objeto de clase Cuenta
+	 *No devuelve nada. Cambia el estado del objeto Cuenta a "Activada"
+	 */
+	public void activarCuenta(Cuenta cuenta) {
+		cuenta.setEstado("Activada");
+	}
+	
 }
