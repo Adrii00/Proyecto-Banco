1bf10ed5b1e8f7b6d65f19328083baf79d32c4db
unknown <adryzg00@gmail.com>
2023-05-08 10:34:12 +0200

    Metodo prestamo terminado

--- prBanco/src/prBanco/Operaciones.java
+++ prBanco/src/prBanco/Operaciones.java
@@ -1,5 +1,7 @@
 package prBanco;
 
-public class Operaciones {
-
+public abstract class Operaciones {
+	public void Prestamo(Cuenta c, double cantidad) {
+		c.setSaldo(c.getSaldo()+cantidad);
+		}
 }
